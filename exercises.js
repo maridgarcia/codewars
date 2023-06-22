69dfb3b5968496f35eff09c892bb0530322e329f
Mariana Garcia <mariana.garcia@beedoo.com.br>
2023-06-22 12:44:30 -0300

    220-06-23

--- exercises.js
+++ exercises.js
@@ -167,3 +167,13 @@ const accum = (str) => {
     return result;
 }
 
+/* Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number. 
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"*/
+const createPhoneNumber = (numbersArray) => {
+    let checkInputType = numbersArray.every((number) => typeof number === 'number');
+    let phoneNumber = `(${numbersArray.join('').substring(0, 3)}) ${numbersArray.join('').substring(3, 6)}-${numbersArray.join('').substring(6, 10)}`;
+    if(checkInputType) {
+        return phoneNumber;
+    }
+}
+
