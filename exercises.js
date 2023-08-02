f0a6b5be85e307d096c52d333450c81bbb12cd74
Mariana Garcia <mariana.garcia@beedoo.com.br>
2023-08-02 13:23:08 -0300

    02-08-2023

--- exercises.js
+++ exercises.js
@@ -141,11 +141,12 @@ const accum = (str) => {
 createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"*/
 const createPhoneNumber = (numbersArray) => {
     let checkInputType = numbersArray.every((number) => typeof number === 'number');
-    let phoneNumber = `(${numbersArray.join('').substring(0, 3)}) ${numbersArray.join('').substring(3, 6)}-${numbersArray.join('').substring(6, 10)}`;
     if(checkInputType) {
-        return phoneNumber;
+        return `(${numbersArray.join('').substring(0, 3)}) ${numbersArray.join('').substring(3, 6)}-${numbersArray.join('').substring(6, 10)}`;
     }
 }
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
 
 /* QuickSort */
 const quickSort = (arr) => {
@@ -251,3 +252,13 @@ const domainName = (url) => {
     return match[1]
   }
 }
+
+/*Write a function toWeirdCase (weirdcase in Ruby) that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased and you need to start over for each word.
+
+The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+Examples:
+toWeirdCase( "String" );//=> returns "StRiNg"
+toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe"*/
+
+
