ebab4e08acb493f987abc68fba430fbc396cfb2b
Mariana Garcia <mariana.garcia@beedoo.com.br>
2023-08-02 13:18:42 -0300

    02-08-2023

--- exercises.js
+++ exercises.js
@@ -233,3 +233,21 @@ const generateHashtag = (str) => {
     return `#${upperCasingStr}`;
 }
 
+/*Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"*/
+
+const domainName = (url) => {
+  let domain = url.replace(/^(https?:\/\/)?/, '');
+  
+  domain = domain.replace(/^www\./, '');
+
+  const regex = /^([a-z0-9-]+)\.[a-z]{2,}/;
+  const match = domain.match(regex);
+  
+  if(match) {
+    return match[1]
+  }
+}
